bb571301fd8c170402b663b6605bce0770441189
jimmy666k <64011397@kmitl.ac.th>
2023-11-22 22:15:39 +0700

    add images

--- ui/pageOne.py
+++ ui/pageOne.py
@@ -1,8 +1,17 @@
 import tkinter as tk
-
+from PIL import Image, ImageTk
 class PageOne(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.pack(fill= "both", expand=True)
+        
+        self.canvas = tk.Canvas(self, width=750, height=500)
+        self.canvas.pack()
+
+        img = Image.open("ui/image/newgraph.png")
+        self.background_image = ImageTk.PhotoImage(img)
+        self.canvas.create_image(0,0, anchor=tk.NW, image=self.background_image)
+        self.canvas.create_oval(50, 50, 150, 150, width=1, outline="green")
         
 
 if __name__ == "__main__":
