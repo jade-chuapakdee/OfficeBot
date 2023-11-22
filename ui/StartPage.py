80e0a895c870c63fb73ec688c8e580d1226746d1
jimmy666k <64011397@kmitl.ac.th>
2023-11-23 01:31:24 +0700

    edit pageone

--- ui/StartPage.py
+++ ui/StartPage.py
@@ -57,22 +57,22 @@ class StartPage(tk.Frame):
         top_frame = tk.Frame(bg="#7F7B82", width=900, height=100)   
         top_frame.place(x=0, y=0) 
         
-        label_top = "OfficeBot"
-        label_top = tk.Label(top_frame, text= label_top,bg='#7F7B82' ,fg="#FFFFFF" , font=("Helvetica",32, "bold"))
+        label_top_text = "OfficeBot"
+        label_top = tk.Label(top_frame, text= label_top_text,bg='#7F7B82' ,fg="#FFFFFF" , font=('Ubuntu',32, "bold"))
         label_top.place(x = 369, y = 22)
         
         label1_text = "Destination"
-        label1 = tk.Label(self, text= label1_text,bg='#E5D0CC',fg="#444554" , font=("Helvetica",22))
+        label1 = tk.Label(self, text= label1_text,bg='#E5D0CC',fg="#444554" , font=('Ubuntu',22, "bold"))
         label1.place(x = 177, y = 174)
 
         # label2
         label2_text = "Sender seat"
-        label2 = tk.Label(self, text= label2_text,bg='#E5D0CC',fg="#444554" , font=("Helvetica",22))
+        label2 = tk.Label(self, text= label2_text,bg='#E5D0CC',fg="#444554" , font=('Ubuntu',22, "bold"))
         label2.place(x = 177, y = 235)
 
         #label3
         label3_text = "Tray number"
-        label3 = tk.Label(self, text= label3_text,bg='#E5D0CC' ,fg="#444554" , font=("Helvetica",22))
+        label3 = tk.Label(self, text= label3_text,bg='#E5D0CC' ,fg="#444554" , font=('Ubuntu',22, "bold"))
         label3.place(x = 177, y = 296)    
         
             
@@ -96,25 +96,25 @@ class StartPage(tk.Frame):
         destination_menu = ttk.Combobox(self, textvariable = destination_option, values = destination_list, width = 30)
         destination_menu.pack(pady=10)
         destination_menu.place(x = 420, y = 176)
-        destination_menu.config(font=("Helvetica", 12),foreground="#444554")
+        destination_menu.config(font=('Ubuntu', 12),foreground="#444554")
         
         sender_menu = ttk.Combobox(self, textvariable = sender_option, values = sender_seat_list, width = 30)
         sender_menu.pack(pady=10)
         sender_menu.place(x = 420, y = 237)
-        sender_menu.config(font=("Helvetica", 12),foreground="#444554")
+        sender_menu.config(font=('Ubuntu', 12),foreground="#444554")
         
         tray_menu = ttk.Combobox(self, textvariable = tray_option, values = tray_number_list, width = 30)
         tray_menu.pack(pady=10)
         tray_menu.place(x = 420, y = 298)
-        tray_menu.config(font=("Helvetica", 12),foreground="#444554")    
+        tray_menu.config(font=('Ubuntu', 12),foreground="#444554")    
             
         #confirm button
-        confirm_button = tk.Button(self, text = "Confirm", command = confirm_button_click, width = 12, height = 1, bg = "#BFACB5", fg = "#02FF3A", font=("Helvetica",18, "bold"), border=2)
+        confirm_button = tk.Button(self, text = "Confirm", command = confirm_button_click, width = 12, height = 1, bg = "#BFACB5", fg = "#02FF3A", font=('Ubuntu',18, "bold"), border=2)
         confirm_button.pack(pady=10)
         confirm_button.place(x = 251, y = 395)
         
         #clear button 
-        clear_button = tk.Button(self, text = "Clear", command = clear_button_click, width = 12, height = 1, bg = "#BFACB5", fg = "#FF0000", font=("Helvetica",18,"bold"), border=2)
+        clear_button = tk.Button(self, text = "Clear", command = clear_button_click, width = 12, height = 1, bg = "#BFACB5", fg = "#FF0000", font=('Ubuntu',18,"bold"), border=2)
         clear_button.pack(pady=10)
         clear_button.place(x = 483, y = 395)   
             
--- ui/pageOne.py
+++ ui/pageOne.py
@@ -4,15 +4,25 @@ class PageOne(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.pack(fill= "both", expand=True)
+        self.configure(bg = "#E5D0CC")
         
-        self.canvas = tk.Canvas(self, width=700, height=394)
+        self.canvas = tk.Canvas(self, width=740, height=421)
         self.canvas.pack()
-        self.canvas.place(x=0, y=0)
+        self.canvas.place(x=0, y=80)
+        
+        top_frame = tk.Frame(bg="#7F7B82", width=900, height=100)   
+        top_frame.place(x=0, y=0) 
+        
+        label_top_text = "Package is on the way"
+        label_top = tk.Label(top_frame, text= label_top_text,bg='#7F7B82' , fg = "#FFFFFF", font=("Helvetica",32, "bold"))
+        label_top.place(x = 46, y = 23)
+        
+        
 
-        img = Image.open("ui/image/graph_2_700x394.png")
+        img = Image.open("ui/image/graph_750x421.png")
         self.background_image = ImageTk.PhotoImage(img)
         self.canvas.create_image(0,0, anchor=tk.NW, image=self.background_image)
-        self.canvas.create_oval(50, 50, 150, 150, width=1, outline="green")
+       
         
 
 if __name__ == "__main__":
