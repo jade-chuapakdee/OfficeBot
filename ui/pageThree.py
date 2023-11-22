ebcbb09303db86633023b543b86bc02440811008
TRP64011655 <90514870+TRP64011655@users.noreply.github.com>
2023-11-23 05:39:44 +0700

    ui updated
    pageThree is the same as pageReject

--- ui/pageThree.py
+++ ui/pageThree.py
@@ -11,16 +11,14 @@ class PageThree(tk.Frame):
             master.switch_frame("StartPage")
 
         label1_text = "Package rejected"
-        label1 = tk.Label(self, text=label1_text, bg='#e6bb95', font=("Helvetica", 16))
+        label1 = tk.Label(self, text=label1_text, bg='#e6bb95', font=("Ubuntu", 16, "bold"))
         label1.place(x=300, y=150)
-
-        # Use master.shared_reason.get() to get the reason
         label2_text = "Reason: " + master.shared_reason.get()
-        label2 = tk.Label(self, text=label2_text, bg='#e6bb95', font=("Helvetica", 16))
+        label2 = tk.Label(self, text=label2_text, bg='#e6bb95', font=("Ubuntu", 16, "bold"))
         label2.place(x=300, y=200)
 
         return_button = tk.Button(self, text="OK", command=on_click_return, width=10, height=1,
-                                  bg="#83c6d2", fg="black", font=("Helvetica", 16, "bold"), borderwidth=1)
+                                  bg="#83c6d2", fg="black", font=("Ubuntu", 16, "bold"), borderwidth=1)
         return_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 if __name__ == "__main__":
--- ui/pageTwo.py
+++ ui/pageTwo.py
@@ -1,60 +1,91 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import simpledialog
 
 class PageTwo(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self.pack(fill= "both", expand=True)
-        self.configure(bg = "#e6bb95")
+        self.pack(fill="both", expand=True)
+        self.configure(bg="#E5D0CC")
+        
+        self.reason = ""
 
+        style = ttk.Style()
+        style.theme_use("combostyle")
+
+        # Variables for labels
+        document_form_label_text = tk.StringVar(self)
+        tray_number_label_text = tk.StringVar(self)
+   
         def on_click_accept():
-            #will define later
             print("accept button clicked")
             master.switch_frame("StartPage")
 
         def on_click_reject():
-            show_input_dialog()
-            print("reject button clicked")
-            # No need to switch frame here; it's already done in show_input_dialog
-            self.master.shared_reason.set(self.reason)
-            self.master.switch_frame("Reject")
-
-        def show_input_dialog():
             result = simpledialog.askstring("Input", "Enter your reason:")
             if result:
                 print("User entered:", result)
                 master.shared_reason.set(result)
-                # master.switch_frame("pageThree")  # Move the switch_frame here
+                self.master.switch_frame("PageReject")
+                
             else:
                 print("User canceled the input.")
+            print("reject button clicked")
+
+        top_frame = tk.Frame(bg="#7F7B82", width=900, height=100)
+        top_frame.place(x=0, y=0)
+
+        label_top = "Your Document is Here!"
+        label_top = tk.Label(top_frame, text=label_top, bg='#7F7B82', fg="#FFFFFF", font=("Ubuntu", 32, "bold"))
+        label_top.place(x=30, y=20)
+
+        label1_text = "Document From:  "
+        label1 = tk.Label(self, text=label1_text, bg='#E5D0CC', fg="#444554", font=("Ubuntu", 22, "bold"))
+        label1.place(x=245, y=174)
         
-        label1_text = "Documents from: " + master.shared_from.get()
-        label1 = tk.Label(self, text= label1_text,bg='#e6bb95' , font=("Helvetica",16))
-        label1.place(x = 165, y = 50)
+        labelDoc_text = master.shared_from.get()
+        labelDoc = tk.Label(self, text=labelDoc_text, bg='#E5D0CC', fg="red", font=("Ubuntu", 22, "bold"))
+        labelDoc.place(x=430, y=174)
 
-        label2_text = "tray number: " + master.shared_tray.get()
-        label2 = tk.Label(self, text= label2_text,bg='#e6bb95' , font=("Helvetica",16))
-        label2.place(x = 165, y = 130)
+        # label2
+        label2_text = "Tray Number:  "
+        label2 = tk.Label(self, text=label2_text, bg='#E5D0CC', fg="#444554", font=("Ubuntu", 22, "bold"))
+        label2.place(x=245, y=225)
+        
+        labelTray_text = master.shared_tray.get()
+        labelTray = tk.Label(self, text=labelTray_text, bg='#E5D0CC', fg="red", font=("Ubuntu", 22, "bold"))
+        labelTray.place(x=400, y=225)
+        
+        # Red Rectangle
+        bottom_frame = tk.Frame(bg="#f48686", width=410, height=100)
+        bottom_frame.place(x=250, y=280)
 
-        bottom_frame = tk.Frame(bg="#f48686", width=400, height=100)
-        bottom_frame.place(x=175, y=200)
-
-        label3_text = "Please click accept after receive package"
-        label3 = tk.Label(bottom_frame, text= label3_text,fg="black", bg="#f48686" , font=("Helvetica",14))
-        label3.place(x = 20, y = 10)
-
-        label4_text = "Please click reject if you are not the receiver"
-        label4 = tk.Label(bottom_frame, text= label4_text,fg="black", bg="#f48686" , font=("Helvetica",14))
-        label4.place(x = 10, y = 50)
+        label3_text = "Please click CONFIRM after receive package"   
+        label3 = tk.Label(bottom_frame, text= label3_text,fg="black", bg="#f48686" , font=("Ubuntu",16, "bold"))
+        label3.place(x = 25, y = 25)
+        
+        label4_text = "Please click REJECT if you are not the reciever"   
+        label4 = tk.Label(bottom_frame, text= label4_text,fg="black", bg="#f48686" , font=("Ubuntu",16, "bold"))
+        label4.place(x = 25, y = 50)
 
 
-        accept_button = tk.Button(self, text = "Confirm", command = on_click_accept, width = 10, height = 1, bg = "#83c6d2", fg = "black", font=("Helvetica",16, "bold"), borderwidth=1)
+        # labels
+        document_form_label = tk.Label(self, textvariable=document_form_label_text, bg='#E5D0CC', font=("Ubuntu", 12, "bold"))
+        document_form_label.pack(pady=10)
+        document_form_label.place(x=420, y=176)
+
+        tray_number_label = tk.Label(self, textvariable=tray_number_label_text, bg='#E5D0CC', font=("Ubuntu", 12, "bold"))
+        tray_number_label.pack(pady=10)
+        tray_number_label.place(x=420, y=237)
+        
+        # accept button
+        accept_button = tk.Button(self, text = "Confirm", command = on_click_accept, width=12, height=1,
+                                 bg="#BFACB5", fg="green", font=("Ubuntu", 18, "bold"), border=2)
         accept_button.pack(pady=10)
-        accept_button.place(x=200, y=350)
-
-        reject_button = tk.Button(self, text = "Reject", command = on_click_reject, width = 10, height = 1, bg = "#83c6d2", fg = "black", font=("Helvetica",16, "bold"), borderwidth=1)
+        accept_button.place(x=250, y=395)
+        
+        # reject button
+        reject_button = tk.Button(self, text = "Reject", command = on_click_reject, width=12, height=1,
+                                 bg="#BFACB5", fg="#FF0000", font=("Ubuntu", 18, "bold"), border=2)
         reject_button.pack(pady=10)
-        reject_button.place(x=400, y=350)
-if __name__ == "__main__":
-    app = PageTwo()
-    app.mainloop()
+        reject_button.place(x=483, y=395)
