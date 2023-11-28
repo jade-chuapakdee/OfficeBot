56bea74e415169c6899855bc4f88ad614d642abc
jimmy666k <64011397@kmitl.ac.th>
2023-11-28 16:27:46 +0700

    update office ui

--- ui/deliverPage.py
+++ ui/deliverPage.py
@@ -15,13 +15,18 @@ class DeliverPage(tk.Frame):
         def on_click_next():
             master.switch_frame("ReachedPage")
             
-        def draw_wall(x1,y1,x2,y2):
-            self.canvas.create_line(x1, y1, x2, y2, fill="red", width=2)
-        
-        def draw_obsatcle(x1,y1,x2,y2):
-            self.canvas.create_line(x1, y1, x2, y2, fill="blue", width=2)
+        def draw_obsatcle(x,y):
+            self.canvas.itemconfig(f"t_{x}_{y}", fill="red")
+            self.canvas.itemconfig(f"b_{x}_{y}" , fill="red")
+            self.canvas.itemconfig(f"l_{x}_{y}", fill="red")
+            self.canvas.itemconfig(f"r_{x}_{y}", fill="red")
             
             
+        def draw_cell(x1,y1,x2,y2,i,j):
+            self.canvas.create_line(x1, y1, x2, y2, fill="#7F7B82", width=2, tags=f"t_{i}_{j}")
+            self.canvas.create_line(x1, y1+52, x2, y2+52, fill="#7F7B82", width=2, tags=f"b_{i}_{j}")
+            self.canvas.create_line(x1, y1, x2-78, y2+52, fill="#7F7B82", width=2, tags=f"l_{i}_{j}")
+            self.canvas.create_line(x1+78, y1, x2, y2+52, fill="#7F7B82", width=2, tags=f"r_{i}_{j}")
             
 
         self.top_frame = tk.Frame(bg="#7F7B82", width=900, height=100)
@@ -37,113 +42,93 @@ class DeliverPage(tk.Frame):
                                 highlightthickness=0)
         next_button.pack()
         next_button.place(x=830, y=35)
-
-        # Define the number of rows and columns
-        rows = 7
-        columns = 9
-
-        # Calculate the row and column heights and widths
-        row_height = self.canvas.winfo_reqheight() / rows
-        column_width = self.canvas.winfo_reqwidth() / columns
-
-        # Draw horizontal grid lines
-        for i in range(1, rows):
-            y = i * row_height
-            self.canvas.create_line(0, y, self.canvas.winfo_reqwidth(), y, fill="#7F7B82")
-
-        # Draw vertical grid lines
-        for i in range(1, columns):
-            x = i * column_width
-            self.canvas.create_line(x, 0, x, self.canvas.winfo_reqheight(), fill="#7F7B82")
-
-        # Draw outline lines
-        self.canvas.create_line(2, 2, 800, 2, fill="#7F7B82", width=2)  # top
-        self.canvas.create_line(2, 2, 2, 320, fill="#7F7B82", width=2)  # left
-        self.canvas.create_line(800, 2, 800, 320, fill="#7F7B82", width=2)  # right
-        self.canvas.create_line(2, 320, 800, 320, fill="#7F7B82", width=2)  # bottom
+        
+            
+        draw_cell(5, 5, 83 , 5, 0, 0)
+        draw_cell(5, 58, 83 , 58, 0, 1)
+        draw_cell(5, 111, 83 , 111, 0, 2)
+        draw_cell(5, 164, 83 , 164, 0, 3)
+        draw_cell(5, 217, 83 , 217, 0, 4)
+        draw_cell(5, 270, 83 , 270, 0, 5)
+        draw_cell(84, 5, 162 , 5, 1, 0)
+        draw_cell(84, 58, 162 , 58, 1, 1)
+        draw_cell(84, 111, 162 , 111, 1, 2)
+        draw_cell(84, 164, 162 , 164, 1, 3)
+        draw_cell(84, 217, 162 , 217, 1, 4)
+        draw_cell(84, 270, 162 , 270, 1, 5)
+        draw_cell(163, 5, 241 , 5, 2, 0)
+        draw_cell(163, 58, 241 , 58, 2, 1)
+        draw_cell(163, 111, 241 , 111, 2, 2)
+        draw_cell(163, 164, 241 , 164, 2, 3)
+        draw_cell(163, 217, 241 , 217, 2, 4)
+        draw_cell(163, 270, 241 , 270, 2, 5)
+        draw_cell(242, 5, 320 , 5, 3, 0)
+        draw_cell(242, 58, 320 , 58, 3, 1)
+        draw_cell(242, 111, 320 , 111, 3, 2)
+        draw_cell(242, 164, 320 , 164, 3, 3)
+        draw_cell(242, 217, 320 , 217, 3, 4)
+        draw_cell(242, 270, 320 , 270, 3, 5)
+        draw_cell(321, 5, 399 , 5, 4, 0)
+        draw_cell(321, 58, 399 , 58, 4, 1)
+        draw_cell(321, 111, 399 , 111, 4, 2)
+        draw_cell(321, 164, 399 , 164, 4, 3)
+        draw_cell(321, 217, 399 , 217, 4, 4)
+        draw_cell(321, 270, 399 , 270, 4, 5)
+        draw_cell(400, 5, 478 , 5, 5, 0)
+        draw_cell(400, 58, 478 , 58, 5, 1)
+        draw_cell(400, 111, 478 , 111, 5, 2)
+        draw_cell(400, 164, 478 , 164, 5, 3)
+        draw_cell(400, 217, 478 , 217, 5, 4)
+        draw_cell(400, 270, 478 , 270, 5, 5)
+        draw_cell(479, 5, 557 , 5, 6, 0)
+        draw_cell(479, 58, 557 , 58, 6, 1)
+        draw_cell(479, 111, 557 , 111, 6, 2)
+        draw_cell(479, 164, 557 , 164, 6, 3)
+        draw_cell(479, 217, 557 , 217, 6, 4)
+        draw_cell(479, 270, 557 , 270, 6, 5)
+        draw_cell(558, 5, 636 , 5, 7, 0)
+        draw_cell(558, 58, 636 , 58, 7, 1)
+        draw_cell(558, 111, 636 , 111, 7, 2)
+        draw_cell(558, 164, 636 , 164, 7, 3)
+        draw_cell(558, 217, 636 , 217, 7, 4)
+        draw_cell(558, 270, 636 , 270, 7, 5)
+        draw_cell(637, 5, 715 , 5, 8, 0)
+        draw_cell(637, 58, 715 , 58, 8, 1)
+        draw_cell(637, 111, 715 , 111, 8, 2)
+        draw_cell(637, 164, 715 , 164, 8, 3)
+        draw_cell(637, 217, 715 , 217, 8, 4)
+        draw_cell(637, 270, 715 , 270, 8, 5)
+        draw_cell(716, 5, 794 , 5, 9, 0)
+        draw_cell(716, 58, 794 , 58, 9, 1)
+        draw_cell(716, 111, 794 , 111, 9, 2)
+        draw_cell(716, 164, 794 , 164, 9, 3)
+        draw_cell(716, 217, 794 , 217, 9, 4)
+        draw_cell(716, 270, 794 , 270, 9, 5)
+        
+        draw_obsatcle(2,0)
+        draw_obsatcle(2,2)
+        draw_obsatcle(2,4)
+        draw_obsatcle(2,5)
+        draw_obsatcle(4,3)
+        draw_obsatcle(6,1)
+        draw_obsatcle(6,2)
+        draw_obsatcle(6,4)
+        draw_obsatcle(7,2)
+        draw_obsatcle(9,4)
+        
+        obasacle = ["2,0","2,2","2,4","2,5","4,3","6,1","6,2","6,4","7,2","9,4"]
+                
+        
+        
         
         #text in cells
-        self.canvas.create_text(400, 160, text="C", font=("Helvetica", 16), fill="blue")
-        self.canvas.create_text(40, 160, text="B", font=("Helvetica", 16), fill="blue")
-        self.canvas.create_text(40, 70, text="A", font=("Helvetica", 16), fill="blue")
-        self.canvas.create_text(40, 250, text="D", font=("Helvetica", 16), fill="blue")
-        self.canvas.create_text(400, 300, text="E", font=("Helvetica", 16), fill="blue")
-        self.canvas.create_text(750, 125, text="F", font=("Helvetica", 16), fill="blue")
-        self.canvas.create_text(750, 215, text="G", font=("Helvetica", 16), fill="blue")
-        
-        #A
-        draw_wall(2, 45, 90 , 45)
-        draw_wall(2, 45, 2 , 95)
-        draw_wall(2, 95, 90 , 95)
-        
-        #B
-        draw_wall(2,140,90,140)
-        draw_wall(2,140,2,185)
-        draw_wall(2,185,90,185)
-        
-        #C
-        draw_wall(358,140,358,185)
-        draw_wall(358,185,448,185)
-        draw_wall(448,140,448,185)
-        
-        #D
-        draw_wall(2,230,90,230)
-        draw_wall(2,230,2,275)
-        draw_wall(2,275,90,275)
-        
-        #E
-        draw_wall(358,280,358,320)
-        draw_wall(358,320,448,320)
-        draw_wall(448,280,448,320)
-        
-        #F
-        draw_wall(715,95,800,95)
-        draw_wall(800,95,800,140)
-        draw_wall(715,140,800,140)
-        
-        #G
-        draw_wall(715,185,800,185)
-        draw_wall(800,185,800,230)
-        draw_wall(715,230,800,230)
-
-        draw_obsatcle(180, 45, 180, 95)
-        draw_obsatcle(265, 45, 265, 95)
-        draw_obsatcle(180,45,265,45)
-        draw_obsatcle(180,95,265,95)
-        
-        draw_obsatcle(180, 140, 180, 185)
-        draw_obsatcle(265, 140, 265, 185)
-        draw_obsatcle(180,140,265,140)
-        draw_obsatcle(180,185,265,185)
-        
-        draw_obsatcle(180, 230, 180, 275)
-        draw_obsatcle(265, 230, 265, 275)
-        draw_obsatcle(180,230,265,230)
-        draw_obsatcle(180,275,265,275)
-        
-        draw_obsatcle(180,280,180,320)
-        draw_obsatcle(265,280,265,320)
-        draw_obsatcle(180,280,265,280)
-        draw_obsatcle(180,320,265,320)
-        
-        draw_obsatcle(360, 1, 360, 45)
-        draw_obsatcle(445, 1, 445, 45)
-        draw_obsatcle(360,2,445,2)
-        draw_obsatcle(360,45,445,45)
-        
-        draw_obsatcle(360, 185, 360, 230)
-        draw_obsatcle(445, 185, 445, 230)
-        draw_obsatcle(360,230,445,230)
-        
-        draw_obsatcle(540, 95, 540, 185)
-        draw_obsatcle(625, 95, 625, 185)
-        draw_obsatcle(540,95,625,95)
-        draw_obsatcle(540,185,625,185)
-        
-        draw_obsatcle(540, 235, 540, 280)
-        draw_obsatcle(625, 235, 625, 280)
-        draw_obsatcle(540,235,625,235)
-        draw_obsatcle(540,280,625,280)
+        self.canvas.create_text(360, 140, text="C", font=("Helvetica", 16), fill="blue")
+        self.canvas.create_text(40, 140, text="B", font=("Helvetica", 16), fill="blue")
+        self.canvas.create_text(40, 30, text="A", font=("Helvetica", 16), fill="blue")
+        self.canvas.create_text(40, 240, text="D", font=("Helvetica", 16), fill="blue")
+        self.canvas.create_text(360, 300, text="E", font=("Helvetica", 16), fill="blue")
+        self.canvas.create_text(750, 90, text="F", font=("Helvetica", 16), fill="blue")
+        self.canvas.create_text(750, 190, text="G", font=("Helvetica", 16), fill="blue")
         
         
     def create_top_text_label(self):
